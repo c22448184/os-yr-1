c83e548829ff59d2934c9823cdeedaaf55637f5e
c22448184 <124065461+c22448184@users.noreply.github.com>
2023-04-27 23:21:32 +0000

    Created a practice file for bash

---
+++ practice-bash/practice-bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+# every bash script starts w shebang
+
+echo "Hello World"
+
+# you can create variables and use them with $ in front, make sure to not have spaces with the = sign
+GREET="My Variable"
+echo $GREET
