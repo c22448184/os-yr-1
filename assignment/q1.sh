435ce9502623311fa87f83ce3cf5ace6f96cd443
c22448184 <124065461+c22448184@users.noreply.github.com>
2023-04-30 12:28:11 +0000

    trying to get display history to work (failed)

--- assignment/q1.sh
+++ assignment/q1.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
-#!/bin/bash
-
 PS3="Please enter a number from 1-6 for your option please: "
 
 select opt in "List files" "Show free disk space" "Show system path" "Display command history" "Backup files" "Exit"
 do
     case $opt in
         "List files")
-            echo "$opt";;
+            echo "$opt - Listing all files in current directory:"
+            ls;;
+
         "Show free disk space")
-            echo "$opt";;
+            echo "$opt:"
+            df -H;;
+
         "Show system path")
-            echo "$opt";;
+            echo "$opt:"
+            echo "$PATH";;
+
         "Display command history")
-            echo "$opt";;
+            echo "$opt:"
+            echo -e $(cat ~/.bash_history);;
+
         "Backup files")
-            echo "$opt";;
+            echo "$opt." 
+            read -p "Please enter the path to your directory: " mydir # read user input for mydir
+            echo "Your directory: $mydir"
+
+            mkdir BackupFolder # make a new backup directory
+            cp -R $mydir BackupFolder # copy recursively the contents of mydir to the backup directory
+            echo "Backup contents:"
+            ls BackupFolder;; # list the contents of the backup directory
+
         "Exit")
             echo "Exiting, thank you."
             break;;
+
         *)
             echo "Invalid option.";;
     esac
