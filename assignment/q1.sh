b7583a4fac5c52c4fc491f240d1adf0dd5e94a26
c22448184 <124065461+c22448184@users.noreply.github.com>
2023-05-01 00:37:35 +0000

    fixing history

--- assignment/q1.sh
+++ assignment/q1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# note that for command history to work properly, you need to close the terminal and then reopen your terminal
+
 PS3="Please enter a number from 1-6 for your option please: "
 
 select opt in "List files" "Show free disk space" "Show system path" "Display command history" "Backup files" "Exit"
@@ -19,15 +21,16 @@ do
 
         "Display command history")
             echo "$opt:"
-            echo -e $(cat ~/.bash_history);;
+            HISTFILE=~/.bash_history # set the variable HISTFILE to the .bash_history file, not quite sure why but it won't work without it
+            set -o history # enable the history feature in the bash shell so that commands will be saved to .bash_history
+            history;; #  display the contents of the history file
 
         "Backup files")
             echo "$opt." 
             read -p "Please enter the path to your directory: " mydir # read user input for mydir
             echo "Your directory: $mydir"
 
-            mkdir BackupFolder # make a new backup directory
-            cp -R $mydir BackupFolder # copy recursively the contents of mydir to the backup directory
+            cp -R $mydir BackupFolder # copy recursively the contents of mydir to the backup directory 
             echo "Backup contents:"
             ls BackupFolder;; # list the contents of the backup directory
 
